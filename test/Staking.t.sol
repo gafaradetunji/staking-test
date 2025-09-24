344ff498d65fc44dbaee3bc987fb832de03d25db
gafaradetunji <gafaradetunji471@gmail.com>
2025-09-24 06:39:39 +0100

    format check

--- test/Staking.t.sol
+++ test/Staking.t.sol
@@ -18,25 +18,14 @@ contract StakingTest is Test {
         vm.startPrank(owner);
         stakingToken = new MockERC20();
         rewardToken = new MockERC20();
-        staking = new StakingRewards(
-            address(stakingToken),
-            address(rewardToken)
-        );
+        staking = new StakingRewards(address(stakingToken), address(rewardToken));
         vm.stopPrank();
     }
 
     function test_alwaysPass() public {
         assertEq(staking.owner(), owner, "Wrong owner set");
-        assertEq(
-            address(staking.stakingToken()),
-            address(stakingToken),
-            "Wrong staking token address"
-        );
-        assertEq(
-            address(staking.rewardsToken()),
-            address(rewardToken),
-            "Wrong reward token address"
-        );
+        assertEq(address(staking.stakingToken()), address(stakingToken), "Wrong staking token address");
+        assertEq(address(staking.rewardsToken()), address(rewardToken), "Wrong reward token address");
 
         assertTrue(true);
     }
@@ -45,10 +34,7 @@ contract StakingTest is Test {
         deal(address(stakingToken), bob, 10e18);
         // start prank to assume user is making subsequent calls
         vm.startPrank(bob);
-        IERC20(address(stakingToken)).approve(
-            address(staking),
-            type(uint256).max
-        );
+        IERC20(address(stakingToken)).approve(address(staking), type(uint256).max);
 
         // we are expecting a revert if we deposit/stake zero
         vm.expectRevert("amount = 0");
@@ -60,18 +46,11 @@ contract StakingTest is Test {
         deal(address(stakingToken), bob, 10e18);
         // start prank to assume user is making subsequent calls
         vm.startPrank(bob);
-        IERC20(address(stakingToken)).approve(
-            address(staking),
-            type(uint256).max
-        );
+        IERC20(address(stakingToken)).approve(address(staking), type(uint256).max);
         uint256 _totalSupplyBeforeStaking = staking.totalSupply();
         staking.stake(5e18);
         assertEq(staking.balanceOf(bob), 5e18, "Amounts do not match");
-        assertEq(
-            staking.totalSupply(),
-            _totalSupplyBeforeStaking + 5e18,
-            "totalsupply didnt update correctly"
-        );
+        assertEq(staking.totalSupply(), _totalSupplyBeforeStaking + 5e18, "totalsupply didnt update correctly");
     }
 
     function test_cannot_withdraw_amount0() public {
@@ -86,16 +65,8 @@ contract StakingTest is Test {
         uint256 userStakebefore = staking.balanceOf(bob);
         uint256 totalSupplyBefore = staking.totalSupply();
         staking.withdraw(2e18);
-        assertEq(
-            staking.balanceOf(bob),
-            userStakebefore - 2e18,
-            "Balance didnt update correctly"
-        );
-        assertLt(
-            staking.totalSupply(),
-            totalSupplyBefore,
-            "total supply didnt update correctly"
-        );
+        assertEq(staking.balanceOf(bob), userStakebefore - 2e18, "Balance didnt update correctly");
+        assertLt(staking.totalSupply(), totalSupplyBefore, "total supply didnt update correctly");
     }
 
     function test_notify_Rewards() public {
@@ -127,18 +98,13 @@ contract StakingTest is Test {
         // trigger first type of flow success
         staking.notifyRewardAmount(100 ether);
         assertEq(staking.rewardRate(), uint256(100 ether) / uint256(1 weeks));
-        assertEq(
-            staking.finishAt(),
-            uint256(block.timestamp) + uint256(1 weeks)
-        );
+        assertEq(staking.finishAt(), uint256(block.timestamp) + uint256(1 weeks));
         assertEq(staking.updatedAt(), block.timestamp);
 
         // Calculate expected remaining rewards
-        uint256 remainingRewards = (staking.finishAt() - block.timestamp) *
-            staking.rewardRate();
+        uint256 remainingRewards = (staking.finishAt() - block.timestamp) * staking.rewardRate();
 
-        uint256 expectedRewardRate = (100 ether + remainingRewards) /
-            uint256(1 weeks);
+        uint256 expectedRewardRate = (100 ether + remainingRewards) / uint256(1 weeks);
 
         console.log("Expected reward", expectedRewardRate);
 
@@ -154,16 +120,8 @@ contract StakingTest is Test {
         //     expectedRewardRate,
         //     "Reward rate mismatch"
         // );
-        assertEq(
-            staking.finishAt(),
-            block.timestamp + 1 weeks,
-            "FinishAt should be extended by duration"
-        );
-        assertEq(
-            staking.updatedAt(),
-            block.timestamp,
-            "UpdatedAt should equal current block.timestamp"
-        );
+        assertEq(staking.finishAt(), block.timestamp + 1 weeks, "FinishAt should be extended by duration");
+        assertEq(staking.updatedAt(), block.timestamp, "UpdatedAt should equal current block.timestamp");
 
         // trigger setRewards distribution revert
         vm.expectRevert("reward duration not finished");
@@ -181,10 +139,7 @@ contract StakingTest is Test {
 
         deal(address(stakingToken), bob, 2 ether);
         vm.startPrank(bob);
-        IERC20(address(stakingToken)).approve(
-            address(staking),
-            type(uint256).max
-        );
+        IERC20(address(stakingToken)).approve(address(staking), type(uint256).max);
         staking.stake(2 ether);
 
         vm.warp(block.timestamp + 500);
@@ -206,20 +161,12 @@ contract StakingTest is Test {
 
         uint256 finishAt = staking.finishAt();
         vm.warp(finishAt + 1);
-        assertEq(
-            staking.lastTimeRewardApplicable(),
-            finishAt,
-            "Should return the time if it is passed"
-        );
+        assertEq(staking.lastTimeRewardApplicable(), finishAt, "Should return the time if it is passed");
     }
 
     function test_rewardPerToken_noSupply() public {
         uint256 rpt = staking.rewardPerToken();
-        assertEq(
-            rpt,
-            0,
-            "With no staking supply, should return stored rewardPerToken"
-        );
+        assertEq(rpt, 0, "With no staking supply, should return stored rewardPerToken");
     }
 
     function test_earned_function() public {
@@ -233,19 +180,12 @@ contract StakingTest is Test {
 
         deal(address(stakingToken), bob, 2 ether);
         vm.startPrank(bob);
-        IERC20(address(stakingToken)).approve(
-            address(staking),
-            type(uint256).max
-        );
+        IERC20(address(stakingToken)).approve(address(staking), type(uint256).max);
         staking.stake(2 ether);
 
         vm.warp(block.timestamp + 200);
         uint256 reward = staking.earned(bob);
-        assertGt(
-            reward,
-            0,
-            "reward should be greater than zero after time elapses"
-        );
+        assertGt(reward, 0, "reward should be greater than zero after time elapses");
         vm.stopPrank();
     }
 }
